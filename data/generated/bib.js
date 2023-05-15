351bd8a66fa957474ad64bbe2b315b261a97ce23
Shivanimk99 <133676544+Shivanimk99@users.noreply.github.com>
2023-05-15 15:54:52 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿const generatedBibEntries = {
+    "1284265": {
+        "author": "Conrad, P. and Foedisch, M.",
+        "booktitle": "32nd Applied Imagery Pattern Recognition Workshop, 2003. Proceedings.",
+        "doi": "10.1109/AIPR.2003.1284265",
+        "number": "",
+        "pages": "157-160",
+        "title": "Performance evaluation of color based road detection using neural nets and support vector machines",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2003"
+    },
+    "6200501": {
+        "author": "Pochappan, Smita S. and Arvind, D.K. and Walsh, Jennifer and Richardson, Alison M. and Herman, Jan",
+        "booktitle": "2012 Ninth International Conference on Wearable and Implantable Body Sensor Networks",
+        "doi": "10.1109/BSN.2012.34",
+        "number": "",
+        "pages": "172-177",
+        "title": "Mobile Clinical Gait Analysis Using Orient Specks",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2012"
+    },
+    "6844831": {
+        "author": "Chen, Yushi and Lin, Zhouhan and Zhao, Xing and Wang, Gang and Gu, Yanfeng",
+        "doi": "10.1109/JSTARS.2014.2329330",
+        "journal": "IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing",
+        "number": "6",
+        "pages": "2094-2107",
+        "title": "Deep Learning-Based Classification of Hyperspectral Data",
+        "type": "ARTICLE",
+        "volume": "7",
+        "year": "2014"
+    },
+    "8013683": {
+        "author": "Isikdogan, Furkan and Bovik, Alan C. and Passalacqua, Paola",
+        "doi": "10.1109/JSTARS.2017.2735443",
+        "journal": "IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing",
+        "number": "11",
+        "pages": "4909-4918",
+        "title": "Surface Water Mapping by Deep Learning",
+        "type": "ARTICLE",
+        "volume": "10",
+        "year": "2017"
+    },
+    "8455008": {
+        "author": "Rajanikanth, J. and Kanth, T. V. Rajini",
+        "booktitle": "2017 International Conference on Current Trends in Computer, Electrical, Electronics and Communication (CTCEEC)",
+        "doi": "10.1109/CTCEEC.2017.8455008",
+        "number": "",
+        "pages": "1121-1125",
+        "title": "An Explorative Data Analysis on Bangalore City Weather with Hybrid Data Mining Techniques using R",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2017"
+    },
+    "8648216": {
+        "author": "Turner, Alexander and Hayes, Stephen",
+        "doi": "10.1109/TBME.2019.2900863",
+        "journal": "IEEE Transactions on Biomedical Engineering",
+        "number": "11",
+        "pages": "3136-3145",
+        "title": "The Classification of Minor Gait Alterations Using Wearable Sensors and Deep Learning",
+        "type": "ARTICLE",
+        "volume": "66",
+        "year": "2019"
+    },
+    "8977659": {
+        "author": "Jain, Anjali and Shakya, Avinash and Khatter, Harsh and Gupta, Amit Kumar",
+        "booktitle": "2019 International Conference on Issues and Challenges in Intelligent Computing Techniques (ICICT)",
+        "doi": "10.1109/ICICT46931.2019.8977659",
+        "number": "",
+        "pages": "1-4",
+        "title": "A smart System for Fake News Detection Using Machine Learning",
+        "type": "INPROCEEDINGS",
+        "volume": "1",
+        "year": "2019"
+    },
+    "9758927": {
+        "author": "Svadasu, Grandhi and Adimoolam, M.",
+        "booktitle": "2022 International Conference on Business Analytics for Technology and Security (ICBATS)",
+        "doi": "10.1109/ICBATS54253.2022.9758927",
+        "number": "",
+        "pages": "1-5",
+        "title": "Spam Detection in Social Media using Artificial Neural Network Algorithm and comparing Accuracy with Support Vector Machine Algorithm",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2022"
+    },
+    "cmc.2021.012941": {
+        "abstract": "Data fusion is a multidisciplinary research area that involves different domains. It is used to attain minimum detection error probability and maximum reliability with the help of data retrieved from multiple healthcare sources. The generation of huge quantity of data from medical devices resulted in the formation of big data during which data fusion techniques become essential. Securing medical data is a crucial issue of exponentially-pacing computing world and can be achieved by Intrusion Detection Systems (IDS). In this regard, since singular-modality is not adequate to attain high detection rate, there is a need exists to merge diverse techniques using decision-based multimodal fusion process. In this view, this research article presents a new multimodal fusion-based IDS to secure the healthcare data using Spark. The proposed model involves decision-based fusion model which has different processes such as initialization, pre-processing, Feature Selection (FS) and multimodal classification for effective detection of intrusions. In FS process, a chaotic Butterfly Optimization (BO) algorithm called CBOA is introduced. Though the classic BO algorithm offers effective exploration, it fails in achieving faster convergence. In order to overcome this, i.e., to improve the convergence rate, this research work modifies the required parameters of BO algorithm using chaos theory. Finally, to detect intrusions, multimodal classifier is applied by incorporating three Deep Learning (DL)-based classification models. Besides, the concepts like Hadoop MapReduce and Spark were also utilized in this study to achieve faster computation of big data in parallel computation platform. To validate the outcome of the presented model, a series of experimentations was performed using the benchmark NSLKDDCup99 Dataset repository. The proposed model demonstrated its effective results on the applied dataset by offering the maximum accuracy of 99.21%, precision of 98.93% and detection rate of 99.59%. The results assured the betterment of the proposed model.",
+        "author": "Phong Thanh Nguyen, Vy Dang Bich Huynh, Khoa Dang Vo, Phuong Thanh Phan, Mohamed Elhoseny, Dac-Nhuong Le",
+        "doi": "10.32604/cmc.2021.012941",
+        "issn": "1546-2226",
+        "journal": "Computers, Materials \\& Continua",
+        "number": "3",
+        "pages": "2555--2571",
+        "title": "Deep Learning Based Optimal Multimodal Fusion Framework for Intrusion Detection Systems for Healthcare Data",
+        "type": "Article",
+        "url": "http://www.techscience.com/cmc/v66n3/41055",
+        "volume": "66",
+        "year": "2021"
+    },
+    "srivastava2021deep": {
+        "author": "Srivastava, Arpan and Jain, Sonakshi and Miranda, Ryan and Patil, Shruti and Pandya, Sharnil and Kotecha, Ketan",
+        "journal": "PeerJ Computer Science",
+        "pages": "e369",
+        "publisher": "PeerJ Inc.",
+        "title": "Deep learning based respiratory sound analysis for detection of chronic obstructive pulmonary disease",
+        "type": "article",
+        "volume": "7",
+        "year": "2021"
+    }
+};
